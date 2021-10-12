49ff46a20e1bd4f2f7b103572a3b76eb5d3c1a8f
Magno Rafael <89099521+magrafa@users.noreply.github.com>
2021-10-12 17:14:06 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,4 +1,3 @@
-var hambuguer  = document.querySelector(".hamburguer");
-hambuguer.addEventListener("click",function(){
-    document.querySelector(".container").classList.toggle("show-menu");
-});
+document.querySelector(".hamburguer").addEventListener("click",()=>
+    document.querySelector(".container").classList.toggle("show-menu")
+);
